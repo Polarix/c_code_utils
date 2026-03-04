05341e64dcec213fef06a49c5857901cfd3bd608
Polarix <xuyulin91@163.com>
2026-03-04 11:40:21 +0800

    修正CMake文件以支持独立库模式与Head-only模式

--- log_utils/src/log_utils.c
+++ log_utils/src/log_utils.c
@@ -1,10 +1,10 @@
 /**
  * @file log_utils.c
- * @brief 独立编译模式下的库实现（仅需定义宏并包含头文件）
+ * @brief 独立库模式下的函数实现
  */
 
-/* 表示正在构建库（用于符号导出） */
-#define LOG_UTILS_BUILD
+/* 独立库模式下，库在构建时会定义LOG_UTILS_BUILD宏 */
+// #define LOG_UTILS_BUILD
 
 /* 触发头文件中的函数定义 */
 #define LOG_UTILS_IMPLEMENTATION
